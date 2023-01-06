3fb6a886b53b24cb0cb9e99a220b1fd7f9914e14
inspxctelement <admin@inspectelement.tech>
2023-01-06 15:15:33 -0500

    chore: development action

--- scripts/gen_docs.py
+++ scripts/gen_docs.py
@@ -15,269 +15,269 @@ TITLE = "Tarkov Changes API Reference"
 
 if __name__ == "__main__":
   doc_markdown = f"""---
-  title: {TITLE}
+title: {TITLE}
 
-  language_tabs:
-    - shell
-    - python
-    - javascript
+language_tabs:
+  - shell
+  - python
+  - javascript
 
-  toc_footers:
-    - <a href=https://tarkov-changes.com/developer'>Sign Up for a Developer Key</a>
+toc_footers:
+  - <a href=https://tarkov-changes.com/developer'>Sign Up for a Developer Key</a>
 
-  includes:
-    - errors
+includes:
+  - errors
 
-  search: true
+search: true
 
-  code_clipboard: true
+code_clipboard: true
 
-  meta:
-    - name: description
-      content: Here lies the documentation for the Tarkov Changes API. A collection of resources about Escape from Tarkov.
-  ---
+meta:
+  - name: description
+    content: Here lies the documentation for the Tarkov Changes API. A collection of resources about Escape from Tarkov.
+---
 
-  # Introduction
+# Introduction
 
-  Welcome to the documentation about the Tarkov Changes API. Stored in our database are various pieces of information about Escape From Tarkov. 
+Welcome to the documentation about the Tarkov Changes API. Stored in our database are various pieces of information about Escape From Tarkov. 
 
-  We have language bindings in Shell, Python, and JavaScript and you can view code examples in the dark area to the right, and you can switch the programming language of the examples with the tabs in the top right.
+We have language bindings in Shell, Python, and JavaScript and you can view code examples in the dark area to the right, and you can switch the programming language of the examples with the tabs in the top right.
 
 
-  # Authentication
+# Authentication
 
-  To use the API please see https://tarkov-changes.com/developer. Once connected, you will set the "AUTH-Token" header as part of every API request.
+To use the API please see https://tarkov-changes.com/developer. Once connected, you will set the "AUTH-Token" header as part of every API request.
 
-  """
+"""
 
   for endpoint in ENDPOINTS:
     builder = SchemaBuilder()
     schema = builder.add_object(requests.get(f"{API_URL}/{endpoint.lower()}", headers=AUTH_HEADER).json())
     doc_markdown += f"""# {endpoint}
 
-  ## Get (all) {endpoint}
+## Get (all) {endpoint}
 
-  ```python
-  import requests
-  import json
+```python
+import requests
+import json
 
-  url = "{API_URL}/{endpoint}"
-  headers = {{
-    'Content-Type': 'application/json',
-    'AUTH-Token': auth_token
-  }}
+url = "{API_URL}/{endpoint}"
+headers = {{
+  'Content-Type': 'application/json',
+  'AUTH-Token': auth_token
+}}
 
-  response = requests.get("GET", url, headers=headers)
+response = requests.get("GET", url, headers=headers)
 
-  print(response.text)
-  ```
+print(response.text)
+```
 
-  ```shell
-  curl "{API_URL}/{endpoint.lower()}" -H "AUTH-Token: $sometoken"
-  ```
+```shell
+curl "{API_URL}/{endpoint.lower()}" -H "AUTH-Token: $sometoken"
+```
 
-  ```javascript
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("AUTH-Token", $auth_token);
+```javascript
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("AUTH-Token", $auth_token);
 
-  var requestOptions = {{
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  }};
+var requestOptions = {{
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+}};
 
-  fetch("{API_URL}/{endpoint}", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  ```
+fetch("{API_URL}/{endpoint}", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+```
 
-  > The above command returns the following structure:
+> The above command returns the following structure:
 
-  ```json
-  {builder.to_json(indent=2)}
-  ```
+```json
+{builder.to_json(indent=2)}
+```
 
-  ### HTTP Request
+### HTTP Request
 
-  `GET https://{API_URL}/{endpoint.lower()}`
+`GET https://{API_URL}/{endpoint.lower()}`
 
-  ## Get a specific {endpoint}
+## Get a specific {endpoint}
 
-  ```python
-  import requests
-  import json
+```python
+import requests
+import json
 
-  url = "{API_URL}/{endpoint}?query=foo"
-  headers = {{
-    'Content-Type': 'application/json',
-    'AUTH-Token': auth_token
-  }}
+url = "{API_URL}/{endpoint}?query=foo"
+headers = {{
+  'Content-Type': 'application/json',
+  'AUTH-Token': auth_token
+}}
 
-  response = requests.get("GET", url, headers=headers)
+response = requests.get("GET", url, headers=headers)
 
-  print(response.text)
-  ```
+print(response.text)
+```
 
-  ```shell
-  curl "{API_URL}/{endpoint.lower()}?query=foo" -H "AUTH-Token: $sometoken"
-  ```
+```shell
+curl "{API_URL}/{endpoint.lower()}?query=foo" -H "AUTH-Token: $sometoken"
+```
 
-  ```javascript
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("AUTH-Token", $auth_token);
+```javascript
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("AUTH-Token", $auth_token);
 
-  var requestOptions = {{
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  }};
+var requestOptions = {{
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+}};
 
-  fetch("{API_URL}/{endpoint}?query=foo", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  ```
+fetch("{API_URL}/{endpoint}?query=foo", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+```
 
-  > The above command returns the following structure:
+> The above command returns the following structure:
 
-  ```json
-  {builder.to_json(indent=2)}
-  ```
+```json
+{builder.to_json(indent=2)}
+```
 
-  ### HTTP Request
+### HTTP Request
 
-  `GET https://{API_URL}/{endpoint.lower()}?query=foo`
+`GET https://{API_URL}/{endpoint.lower()}?query=foo`
 
-  ### Query Parameters
+### Query Parameters
 
-  Parameter | Default | Description
-  --------- | ------- | -----------
-  query | false | Include the item or search term you wish to search for
+Parameter | Default | Description
+--------- | ------- | -----------
+query | false | Include the item or search term you wish to search for
 
-  """
+"""
 
   # Search
   builder = SchemaBuilder()
   schema = builder.add_object(requests.get(f"{API_URL}/search?query=killa", headers=AUTH_HEADER).json())
   endpoint = "search"
   doc_markdown += f"""
-  # Search
+# Search
 
-  ## Search for a specific item
+## Search for a specific item
 
-  ```python
-  import requests
-  import json
+```python
+import requests
+import json
 
-  url = "{API_URL}/{endpoint}?query=foo"
-  headers = {{
-    'Content-Type': 'application/json',
-    'AUTH-Token': auth_token
-  }}
+url = "{API_URL}/{endpoint}?query=foo"
+headers = {{
+  'Content-Type': 'application/json',
+  'AUTH-Token': auth_token
+}}
 
-  response = requests.get("GET", url, headers=headers)
+response = requests.get("GET", url, headers=headers)
 
-  print(response.text)
-  ```
+print(response.text)
+```
 
-  ```shell
-  curl "{API_URL}/{endpoint.lower()}?query=foo" -H "AUTH-Token: $sometoken"
-  ```
+```shell
+curl "{API_URL}/{endpoint.lower()}?query=foo" -H "AUTH-Token: $sometoken"
+```
 
-  ```javascript
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("AUTH-Token", $auth_token);
+```javascript
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("AUTH-Token", $auth_token);
 
-  var requestOptions = {{
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  }};
+var requestOptions = {{
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+}};
 
-  fetch("{API_URL}/{endpoint}?query=foo", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  ```
+fetch("{API_URL}/{endpoint}?query=foo", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+```
 
-  > The above command returns the following structure:
+> The above command returns the following structure:
 
-  ```json
-  {builder.to_json(indent=2)}
-  ```
+```json
+{builder.to_json(indent=2)}
+```
 
-  ### HTTP Request
+### HTTP Request
 
-  `GET {API_URL}/search?query=killa`
+`GET {API_URL}/search?query=killa`
 
-  ### Query Parameters
+### Query Parameters
 
-  Parameter | Default | Description
-  --------- | ------- | -----------
-  query | false | Include the item or search term you wish to search for
+Parameter | Default | Description
+--------- | ------- | -----------
+query | false | Include the item or search term you wish to search for
 
-  """
+"""
 
   # Weather
   builder = SchemaBuilder()
   schema = builder.add_object(requests.get(f"{API_URL}/weather", headers=AUTH_HEADER).json())
   endpoint = "weather"
   doc_markdown += f"""
-  # Weather
+# Weather
 
-  ## Get up to date weather information
+## Get up to date weather information
 
-  ```python
-  import requests
-  import json
+```python
+import requests
+import json
 
-  url = "{API_URL}/{endpoint}"
-  headers = {{
-    'Content-Type': 'application/json',
-    'AUTH-Token': auth_token
-  }}
+url = "{API_URL}/{endpoint}"
+headers = {{
+  'Content-Type': 'application/json',
+  'AUTH-Token': auth_token
+}}
 
-  response = requests.get("GET", url, headers=headers)
+response = requests.get("GET", url, headers=headers)
 
-  print(response.text)
-  ```
+print(response.text)
+```
 
-  ```shell
-  curl "{API_URL}/{endpoint.lower()}" -H "AUTH-Token: $sometoken"
-  ```
+```shell
+curl "{API_URL}/{endpoint.lower()}" -H "AUTH-Token: $sometoken"
+```
 
-  ```javascript
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("AUTH-Token", $auth_token);
+```javascript
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("AUTH-Token", $auth_token);
 
-  var requestOptions = {{
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-  }};
+var requestOptions = {{
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+}};
 
-  fetch("{API_URL}/{endpoint}", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  ```
+fetch("{API_URL}/{endpoint}", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+```
 
-  > The above command returns the following structure:
+> The above command returns the following structure:
 
-  ```json
-  {builder.to_json(indent=2)}
-  ```
+```json
+{builder.to_json(indent=2)}
+```
 
-  ### HTTP Request
+### HTTP Request
 
-  `GET {API_URL}/{endpoint}`
+`GET {API_URL}/{endpoint}`
 
-  """
+"""
   print (doc_markdown)
 
 
